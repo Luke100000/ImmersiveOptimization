df61a9d25a957f0bce61f7a1875bc824fa3268bb
Luke100000 <jonas.peche@aon.at>
2026-05-14 16:03:47 +0200

    ported

--- neoforge/src/main/java/net/conczin/immersive_optimization/ImmersiveOptimization.java
+++ neoforge/src/main/java/net/conczin/immersive_optimization/ImmersiveOptimization.java
@@ -7,6 +7,7 @@ import net.neoforged.neoforge.common.NeoForge;
 @Mod(Constants.MOD_ID)
 public class ImmersiveOptimization {
     public ImmersiveOptimization(IEventBus IEventBus) {
+        CommonClass.forcedChunkLookup = new NeoForgeForcedChunkLookup();
         CommonClass.init();
         NeoForge.EVENT_BUS.register(new NeoForgeBusEvents());
     }
--- neoforge/src/main/java/net/conczin/immersive_optimization/NeoForgeForcedChunkLookup.java
+++ neoforge/src/main/java/net/conczin/immersive_optimization/NeoForgeForcedChunkLookup.java
@@ -3,11 +3,11 @@ package net.conczin.immersive_optimization;
 import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ForcedChunksSavedData;
-import net.minecraftforge.common.world.ForgeChunkManager;
+import net.neoforged.neoforge.common.world.chunk.ForcedChunkManager;
 
 import java.util.Map;
 
-public class ForgeForcedChunkLookup implements CommonClass.ForcedChunkLookup {
+public class NeoForgeForcedChunkLookup implements CommonClass.ForcedChunkLookup {
     @Override
     public boolean isForced(ServerLevel level, long chunk) {
         if (level.getForcedChunks().contains(chunk)) {
@@ -22,7 +22,7 @@ public class ForgeForcedChunkLookup implements CommonClass.ForcedChunkLookup {
         return contains(data.getBlockForcedChunks(), chunk) || contains(data.getEntityForcedChunks(), chunk);
     }
 
-    private static <T extends Comparable<? super T>> boolean contains(ForgeChunkManager.TicketTracker<T> tracker, long chunk) {
+    private static <T extends Comparable<? super T>> boolean contains(ForcedChunkManager.TicketTracker<T> tracker, long chunk) {
         return contains(tracker.getChunks(), chunk) || contains(tracker.getTickingChunks(), chunk);
     }
 
